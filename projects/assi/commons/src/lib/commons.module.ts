6973b109e2f8f177f78549ddcf5ec46d3c6c018f
vgrados <vgrados2@outlook.com>
2020-02-04 00:36:47 -0500

    INICIO DEL PROYECTO

---
+++ projects/assi/commons/src/lib/commons.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonsComponent } from './commons.component';
+
+
+
+@NgModule({
+  declarations: [CommonsComponent],
+  imports: [
+  ],
+  exports: [CommonsComponent]
+})
+export class CommonsModule { }
